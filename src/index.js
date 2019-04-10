9cc2d348d25a7d6ea3dc28c4b73462a8890933bd
Hussain Abbas <habbas@keyhole.co>
2019-04-10 13:10:45 -0400

    Comments on index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@
  * TODO: programmatically import and export all modules
  */
 
+// Atoms
 import Button from './atoms/Button'
 import ColorBlock from './atoms/ColorBlock'
 import ProfilePicture from './atoms/ProfilePicture'
 
+// Molecules
+// Organisms
+// Templates
+
+// ##############################
+
+// Atoms
 export { Button }
 export { ColorBlock }
 export { ProfilePicture }
 
+// Molecules
+// Organisms
+// Templates
+
+// ##############################
+
 export default {
+  // Atoms
   Button,
   ColorBlock,
   ProfilePicture,
+
+  // Molecules
+  // Organisms
+  // Templates
 }
