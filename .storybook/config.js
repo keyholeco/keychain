d68ab5216453af324aae627ba850b7502c10838b
Joann Lai <jlai@keyhole.co>
2019-04-10 14:37:38 -0400

    install viewport addon, add custom keyhole viewports based on GA screen res stats

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { addDecorator, addParameters, configure } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 import { withA11y } from '@storybook/addon-a11y'
 import keyholeTheme from './keyholeTheme'
+import keyholeViewports from './keyholeViewports'
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /\.stories\.js$/)
@@ -17,5 +18,7 @@ addParameters({
 
 addDecorator(withInfo) // info addon must sit on top of other decorators to work properly
 addDecorator(withA11y)
+
+addParameters(keyholeViewports)
 
 configure(loadStories, module)
