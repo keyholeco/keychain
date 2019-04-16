0655a9876f829308bad0ed87752658baf445a61b
Hussain Abbas <habbas@keyhole.co>
2019-04-16 11:59:56 -0400

    Add Header.Navigation and Header.Navigation.Link components.

--- src/organisms/Header/index.js
+++ src/organisms/Header/index.js
@@ -1,8 +1,10 @@
 import Header from './Header'
 import HeaderLogo from './HeaderLogo'
 import HeaderSection from './HeaderSection'
+import HeaderNavigation from './HeaderNavigation'
 
 Header.Logo = HeaderLogo
 Header.Section = HeaderSection
+Header.Navigation = HeaderNavigation
 
 export default Header
