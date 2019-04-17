7b0ae6198968bef00863d6865aa0258f0639935a
Hussain Abbas <habbas@keyhole.co>
2019-04-17 10:46:37 -0400

    Add vertical align stretch property value to Header.Section. Add more stories and comments to Header.Section to be clear. Add advanced Header story to showcase what the header would look like in the Keyhole app.

--- src/organisms/Header/header.section.stories.js
+++ src/organisms/Header/header.section.stories.js
@@ -4,22 +4,24 @@ import { storiesOf } from '@storybook/react'
 import Header from './index'
 import { Type } from '../../../dist'
 
+const sectionStyles = { backgroundColor: 'rgba(0,0,0,0.2)', padding: '0.25em' }
+
 storiesOf('Organisms/Header.Section', module)
   .add('basic', () => {
     return (
       <Header>
         <Header.Logo />
-        <Header.Section>
+        <Header.Section style={sectionStyles}>
           <Type>Here is another section on the header</Type>
         </Header.Section>
       </Header>
     )
   })
-  .add('alignment', () => {
+  .add('horizontal alignment', () => {
     return (
       <Header>
         <Header.Logo />
-        <Header.Section align="right">
+        <Header.Section align="right" style={sectionStyles}>
           <Type>Here is another section on the header</Type>
         </Header.Section>
       </Header>
@@ -29,8 +31,33 @@ storiesOf('Organisms/Header.Section', module)
     return (
       <Header>
         <Header.Logo />
-        <Header.Section align="center" verticalAlign="middle">
-          <Type>Here is another section on the header</Type>
+        <Header.Section align="center" verticalAlign="stretch" style={sectionStyles}>
+          <Type>stretch</Type>
+        </Header.Section>
+        <Header.Section align="center" verticalAlign="top" style={sectionStyles}>
+          <Type>top</Type>
+        </Header.Section>
+        <Header.Section align="center" verticalAlign="middle" style={sectionStyles}>
+          <Type>middle</Type>
+        </Header.Section>
+        <Header.Section align="center" verticalAlign="bottom" style={sectionStyles}>
+          <Type>bottom</Type>
+        </Header.Section>
+      </Header>
+    )
+  })
+  .add('multiple sections', () => {
+    return (
+      <Header>
+        <Header.Logo />
+        <Header.Section align="left" verticalAlign="top" style={sectionStyles}>
+          <Type>Left + Top Aligned</Type>
+        </Header.Section>
+        <Header.Section align="center" verticalAlign="middle" style={sectionStyles}>
+          <Type>Center + Middle Aligned</Type>
+        </Header.Section>
+        <Header.Section align="right" verticalAlign="bottom" style={sectionStyles}>
+          <Type>Right + Bottom Aligned</Type>
         </Header.Section>
       </Header>
     )
--- src/organisms/Header/header.stories.js
+++ src/organisms/Header/header.stories.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 import Header from './index'
+import { Icon, Type, Button } from '../../../dist'
 
 storiesOf('Organisms/Header', module)
   .add('basic', () => {
@@ -15,6 +17,58 @@ storiesOf('Organisms/Header', module)
     return (
       <Header backgroundColor="#26293C">
         <Header.Logo theme="dark" />
+      </Header>
+    )
+  })
+  .add('advanced', () => {
+    return (
+      <Header backgroundColor="#26293C">
+        <Header.Logo theme="dark" />
+
+        <Header.Section align="left" style={{ marginLeft: '3em' }}>
+          <Header.Navigation>
+            <Header.Navigation.Link
+              label="Brand IQ"
+              icon="star"
+              active={true}
+              onClick={action('navigation-link-click')}
+            />
+            <Header.Navigation.Link
+              label="Advanced Suite"
+              icon="lock"
+              onClick={action('navigation-link-click')}
+            />
+          </Header.Navigation>
+        </Header.Section>
+
+        <Header.Section
+          align="right"
+          verticalAlign="middle"
+          style={{
+            fontSize: '0.8em',
+            marginRight: '1rem',
+          }}
+        >
+          <Button
+            onClick={action('button-click')}
+            color="orange"
+            style={{ width: '110px', whiteSpace: 'nowrap' }}
+            caps
+          >
+            <Icon icon="bolt" style={{ marginRight: '0.5em', fontSize: '0.9em' }} />
+            <Type bold>Upgrade</Type>
+          </Button>
+        </Header.Section>
+
+        <Header.Section>
+          <Header.Box label="info@keyhole.co" open={false}>
+            <Header.Box.Dropdown>
+              <Header.Box.Dropdown.Link label="Settings" />
+              <Header.Box.Dropdown.Link label="Administrator" />
+              <Header.Box.Dropdown.Link label="Logout" />
+            </Header.Box.Dropdown>
+          </Header.Box>
+        </Header.Section>
       </Header>
     )
   })
