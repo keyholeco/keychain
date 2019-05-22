b5a6b96c6ede4987fb568b753728d217f8acbc62
Hussain Abbas <habbas@keyhole.co>
2019-05-22 13:55:14 -0400

    Add knobs for Icon component.

--- src/atoms/Icon/icon.stories.js
+++ src/atoms/Icon/icon.stories.js
@@ -1,40 +1,116 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withKnobs, text, object, optionsKnob, boolean } from '@storybook/addon-knobs'
 
-import Icon, { iconSizes } from './index'
+import Icon, { iconTypes, iconSizes } from './index'
 import { Type, H1 } from '../../../dist'
 
 const parentStyles = { color: '#26293C' }
 const iconStyles = { margin: '0.25em' }
 const labelStyles = { display: 'inline-block', width: '150px', textAlign: 'right' }
 
+const iconTypesObj = iconTypes.reduce((prev, curr) => {
+  prev[curr] = curr
+  return prev
+}, {})
+
+const iconSizesObj = iconSizes.reduce((prev, curr) => {
+  prev[curr] = curr
+  return prev
+}, {})
+
+const optionsKnobOptions = {
+  display: 'select',
+}
+
 storiesOf('Atoms/Icon', module)
-  .add('basic', () => (
-    <H1 style={parentStyles}>
-      <Icon icon="users" />
-    </H1>
-  ))
-  .add('brands', () => (
-    <H1 style={parentStyles}>
-      <Icon icon="twitter" type="brands" style={iconStyles} />
-      <Icon icon="instagram" type="brands" style={iconStyles} />
-      <Icon icon="facebook" type="brands" style={iconStyles} />
-      <Icon icon="youtube" type="brands" style={iconStyles} />
-    </H1>
-  ))
+  .addDecorator(withKnobs)
+  .add('basic', () => {
+    return (
+      <H1 style={parentStyles}>
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          icon={text('icon', 'users')}
+          type={optionsKnob('type', iconTypesObj, 'solid', optionsKnobOptions)}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+        />
+      </H1>
+    )
+  })
+  .add('brands', () => {
+    return (
+      <H1 style={parentStyles}>
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          type={optionsKnob('type', iconTypesObj, 'brands', optionsKnobOptions)}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+          icon={text('icon 1', 'twitter')}
+        />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          type={optionsKnob('type', iconTypesObj, 'brands', optionsKnobOptions)}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+          icon={text('icon 2', 'instagram')}
+        />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          type={optionsKnob('type', iconTypesObj, 'brands', optionsKnobOptions)}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+          icon={text('icon 3', 'facebook')}
+        />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          type={optionsKnob('type', iconTypesObj, 'brands', optionsKnobOptions)}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+          icon={text('icon 4', 'youtube')}
+        />
+      </H1>
+    )
+  })
   .add('types', () => (
     <H1 style={parentStyles}>
       <div>
         <span style={labelStyles}>Solid:</span>
-        <Icon icon="users" type="solid" style={iconStyles} />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          icon={text('icon', 'users')}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+          type={optionsKnob('type 1', iconTypesObj, 'solid', optionsKnobOptions)}
+        />
       </div>
       <div>
         <span style={labelStyles}>Regular:</span>
-        <Icon icon="users" type="regular" style={iconStyles} />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          icon={text('icon', 'users')}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+          type={optionsKnob('type 2', iconTypesObj, 'regular', optionsKnobOptions)}
+        />
       </div>
       <div>
         <span style={labelStyles}>Light:</span>
-        <Icon icon="users" type="light" style={iconStyles} />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          icon={text('icon', 'users')}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', false)}
+          type={optionsKnob('type 3', iconTypesObj, 'light', optionsKnobOptions)}
+        />
       </div>
     </H1>
   ))
@@ -43,7 +119,14 @@ storiesOf('Atoms/Icon', module)
       {[...iconSizes].reverse().map((size) => (
         <div key={`icon-size-${size}`}>
           <span style={labelStyles}>{size}:</span>
-          <Icon icon="users" size={size} style={iconStyles} />
+          <Icon
+            className={text('className', '')}
+            style={object('style', iconStyles)}
+            icon={text('icon', 'users')}
+            type={optionsKnob('type', iconTypesObj, 'solid', optionsKnobOptions)}
+            fixed={boolean('fixed', false)}
+            size={optionsKnob(`size ${size}`, iconSizesObj, size, optionsKnobOptions)}
+          />
         </div>
       ))}
     </Type>
@@ -51,13 +134,34 @@ storiesOf('Atoms/Icon', module)
   .add('fixed width', () => (
     <H1 style={parentStyles}>
       <div>
-        <Icon icon="users" type="solid" style={iconStyles} fixed />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          icon={text('icon', 'users')}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', true)}
+          type={optionsKnob('type 1', iconTypesObj, 'solid', optionsKnobOptions)}
+        />
       </div>
       <div>
-        <Icon icon="users" type="regular" style={iconStyles} fixed />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          icon={text('icon', 'users')}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', true)}
+          type={optionsKnob('type 2', iconTypesObj, 'regular', optionsKnobOptions)}
+        />
       </div>
       <div>
-        <Icon icon="users" type="light" style={iconStyles} fixed />
+        <Icon
+          className={text('className', '')}
+          style={object('style', iconStyles)}
+          icon={text('icon', 'users')}
+          size={optionsKnob('size', iconSizesObj, 'lg', optionsKnobOptions)}
+          fixed={boolean('fixed', true)}
+          type={optionsKnob('type 3', iconTypesObj, 'light', optionsKnobOptions)}
+        />
       </div>
     </H1>
   ))
