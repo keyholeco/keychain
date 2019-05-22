ab27fca6a96679a09197a9dda09db38d21b47b48
Hussain Abbas <habbas@keyhole.co>
2019-05-22 13:55:47 -0400

    Add knobs for Type component.

--- src/atoms/Type/index.js
+++ src/atoms/Type/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const weights = [300, 400, 500, 700, 900]
+export const weights = [300, 400, 500, 700, 900, '300', '400', '500', '700', '900']
 export const sizes = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl']
 
 export const Type = (props) => {
@@ -12,14 +12,14 @@ export const Type = (props) => {
     ${props.italics ? 'kc-type--italics' : ''}
     ${props.heading ? 'kc-type--heading' : ''}
     ${props.caps ? 'kc-type--caps' : ''}
-    ${props.weight ? `kc-type--${props.weight}` : ''}
+    ${props.weight ? `kc-type--${props.weight.toString()}` : ''}
     ${props.size ? `kc-type--${props.size}` : ''}
     ${props.className || ''}
   `
 
   const styles = { ...props.style }
   if (props.weight) {
-    styles.fontWeight = props.weight
+    styles.fontWeight = parseInt(props.weight)
   }
 
   switch (props.size) {
