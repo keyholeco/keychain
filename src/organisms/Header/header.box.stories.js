428894f2413f9c31c13933d90da6f08c2987c4f9
Hussain Abbas <habbas@keyhole.co>
2019-05-23 10:07:01 -0400

    Add knobs for HeaderBox component.

--- src/organisms/Header/header.box.stories.js
+++ src/organisms/Header/header.box.stories.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 
 import Header from './index'
 
 const headerStyles = { marginTop: '3em' } // to avoid touching show info button
 
 storiesOf('Organisms/Header.Box', module)
+  .addDecorator(withKnobs)
   .add('basic', () => {
     return (
       <Header backgroundColor="#26293C" style={headerStyles}>
         <Header.Logo theme="dark" />
         <Header.Section align="right" verticalAlign="middle">
-          <Header.Box label="info@keyhole.co" />
+          <Header.Box label={text('label', 'info@keyhole.co')} open={boolean('open', false)}>
+            <Header.Box.Dropdown>
+              <Header.Box.Dropdown.Link
+                label={text('dropdown link label #1', 'Settings')}
+                onClick={action('header-box-dropdown-link-click')}
+              />
+              <Header.Box.Dropdown.Link
+                label={text('dropdown link label #2', 'Administrator')}
+                onClick={action('header-box-dropdown-link-click')}
+              />
+              <Header.Box.Dropdown.Link
+                label={text('dropdown link label #3', 'Logout')}
+                onClick={action('header-box-dropdown-link-click')}
+              />
+            </Header.Box.Dropdown>
+          </Header.Box>
         </Header.Section>
       </Header>
     )
@@ -22,18 +39,18 @@ storiesOf('Organisms/Header.Box', module)
       <Header backgroundColor="#26293C" style={headerStyles}>
         <Header.Logo theme="dark" />
         <Header.Section align="right" verticalAlign="middle">
-          <Header.Box label="info@keyhole.co" open={true}>
+          <Header.Box label={text('label', 'info@keyhole.co')} open={boolean('open', true)}>
             <Header.Box.Dropdown>
               <Header.Box.Dropdown.Link
-                label="Settings"
+                label={text('dropdown link label #1', 'Settings')}
                 onClick={action('header-box-dropdown-link-click')}
               />
               <Header.Box.Dropdown.Link
-                label="Administrator"
+                label={text('dropdown link label #2', 'Administrator')}
                 onClick={action('header-box-dropdown-link-click')}
               />
               <Header.Box.Dropdown.Link
-                label="Logout"
+                label={text('dropdown link label #3', 'Logout')}
                 onClick={action('header-box-dropdown-link-click')}
               />
             </Header.Box.Dropdown>
