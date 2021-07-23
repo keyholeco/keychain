df676a22724754538b79d5e21a8dcbe92581b357
Joann Lai <laijoann@users.noreply.github.com>
2021-07-22 21:45:25 -0400

    migration: rewrite type story

--- src/atoms/Type/type.stories.js
+++ src/atoms/Type/type.stories.js
@@ -1,70 +1,57 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { withKnobs, text, object, optionsKnob, boolean } from '@storybook/addon-knobs'
 
-import Type, { H1, H2, H3, H4, weights, sizes } from './index'
+import Type, { H1, H2, H3, H4 } from './index'
 
 const copy = 'Every great relationship starts with listening.'
 
-const weightsObj = weights.reduce((prev, curr) => {
-  prev[curr.toString()] = curr
-  return prev
-}, {})
-
-const sizesObj = sizes.reduce((prev, curr) => {
-  prev[curr] = curr
-  return prev
-}, {})
-
-const optionsKnobOptions = {
-  display: 'select',
+export default {
+  component: Type,
+  title: 'Atoms/Type',
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '1em' }}>
+        <Story />
+      </div>
+    ),
+  ],
 }
 
-storiesOf('Atoms/Type', module)
-  .addDecorator(withKnobs)
-  .add('basic', () => (
-    <Type
-      className={text('className', '')}
-      style={object('style', {})}
-      bold={boolean('bold', false)}
-      light={boolean('light', false)}
-      italics={boolean('italics', false)}
-      heading={boolean('heading', false)}
-      caps={boolean('caps', false)}
-      weight={optionsKnob(`weight`, weightsObj, '500', optionsKnobOptions)}
-      size={optionsKnob(`size`, sizesObj, 'sm', optionsKnobOptions)}
-    >
-      {text('copy', copy)}
-    </Type>
-  ))
-  .add('bold', () => <Type bold={boolean('bold', true)}>{text('copy', copy)}</Type>)
-  .add('light', () => <Type light={boolean('light', true)}>{text('copy', copy)}</Type>)
-  .add('italics', () => <Type italics={boolean('italics', true)}>{text('copy', copy)}</Type>)
-  .add('heading', () => <Type heading={boolean('heading', true)}>{text('copy', copy)}</Type>)
-  .add('caps', () => <Type caps={boolean('caps', true)}>{text('copy', copy)}</Type>)
-  .add('weight', () => (
-    <div>
-      {weights.map((weight) => (
-        <div key={`type-weight-${weight}`}>
-          <Type weight={optionsKnob(`weight ${weight}`, weightsObj, weight, optionsKnobOptions)}>
-            {weight}: {text('copy', copy)}
-          </Type>
-        </div>
-      ))}
-    </div>
-  ))
-  .add('size', () => (
-    <div>
-      {[...sizes].reverse().map((size) => (
-        <div key={`type-size-${size}`}>
-          <Type size={optionsKnob(`size ${size}`, sizesObj, size, optionsKnobOptions)}>
-            {size}: {text('copy', copy)}
-          </Type>
-        </div>
-      ))}
-    </div>
-  ))
-  .add('h1', () => <H1>{text('copy', copy)}</H1>)
-  .add('h2', () => <H2>{text('copy', copy)}</H2>)
-  .add('h3', () => <H3>{text('copy', copy)}</H3>)
-  .add('h4', () => <H4>{text('copy', copy)}</H4>)
+const Template = (args) => {
+  const TagType = args.tagType || Type
+  return <TagType children={copy} {...args} />
+}
+
+export const Default = Template.bind({})
+
+export const Bold = Template.bind({})
+Bold.args = { bold: true }
+
+export const Light = Template.bind({})
+Light.args = { light: true }
+
+export const Italics = Template.bind({})
+Italics.args = { italics: true }
+
+export const Heading = Template.bind({})
+Heading.args = { heading: true }
+
+export const Caps = Template.bind({})
+Caps.args = { caps: true }
+
+export const Weight = Template.bind({})
+Weight.args = { weight: 300 }
+
+export const Size = Template.bind({})
+Size.args = { size: 'xxxl' }
+
+export const h1 = Template.bind({})
+h1.args = { tagType: H1 }
+
+export const h2 = Template.bind({})
+h2.args = { tagType: H2 }
+
+export const h3 = Template.bind({})
+h3.args = { tagType: H3 }
+
+export const h4 = Template.bind({})
+h4.args = { tagType: H4 }
