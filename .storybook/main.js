aedfbeefb3492c284ea74c98c7f2fd28a94b5352
Joann Lai <laijoann@users.noreply.github.com>
2021-07-23 19:23:53 -0400

    migration: remove addon knobs
    completed migration over to using controls instead of the now deprecated package knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-knobs', '@storybook/addon-a11y', '@storybook/addon-links']
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-links']
 }
